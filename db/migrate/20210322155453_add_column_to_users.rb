cadb5c867a8b78387e651013d5e21c4050d89028
Ali-crewlogix <ali.ahmed@crewlogix.com>
2021-03-23 09:12:57 -0700

    rails admin

--- db/migrate/20210322155453_add_column_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :users, :full_name, :string  	
-  	add_column :users, :uid, :string  	
-  	add_column :users, :avatar_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_151949) do
+ActiveRecord::Schema.define(version: 2021_03_23_160826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_151949) do
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
