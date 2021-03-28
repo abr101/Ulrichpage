b277f17dd394625f9e00994e67f4d5973058ce7e
Ali-crewlogix <ali.ahmed@crewlogix.com>
2021-03-28 11:39:12 -0700

    ckeditor done

--- app/models/post_ad.rb
+++ app/models/post_ad.rb
@@ -1,10 +1,10 @@
 class PostAd < ApplicationRecord
   mount_uploaders :avatars, ImageUploader
-  belongs_to :sub_category
-  belongs_to :continent
-  belongs_to :country
-  belongs_to :state
-  belongs_to :city
-  belongs_to :category
-  belongs_to :user
+  # belongs_to :sub_category
+  # belongs_to :continent
+  # belongs_to :country
+  # belongs_to :state
+  # belongs_to :city
+  # belongs_to :category
+  # belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :sub_categories
   resources :categories
   resources :cities
