6e35c93f5b4a34cdf4e27796f7a80cc223453abf
abr101 <arasheed172@gmail.com>
2021-03-29 20:54:39 +0500

    dashboard ui changes

--- db/schema.rb
+++ db/schema.rb
@@ -46,16 +46,6 @@ ActiveRecord::Schema.define(version: 2021_03_28_182011) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "city_id"
@@ -130,15 +120,16 @@ ActiveRecord::Schema.define(version: 2021_03_28_182011) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "username"
+    t.string "email"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "full_name"
     t.string "uid"
     t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "is_admin", default: false
     t.json "avatars"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
