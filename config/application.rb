c57d1635295fdd32106dd29aa33525a006cac29f
abr101 <arasheed172@gmail.com>
2021-03-29 21:33:16 +0500

    assests precompile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Ulrichpage
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
