8de0bab9647552b738dbfebeeb7b6c4e10928534
abr101 <arasheed172@gmail.com>
2021-05-04 10:01:41 +0500

    add logo

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,7 @@
 class PostMailer < ApplicationMailer
 
 	def new_post_email(email)
-	 mail(to: email, subject: "Successfully posted ad!")
+		@image= File.open("#{Rails.root}/app/assets/images/logo.png")
+	    mail(to: email, subject: "Successfully posted ad!")
 	end
 end
